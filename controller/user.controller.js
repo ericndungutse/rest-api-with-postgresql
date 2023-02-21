14d2c2fa521e325c0255f024b1f65fc03fc286b9
ericndungutse <dav.ndungutse@gmail.com>
2023-02-21 11:10:19 +0200

    feature(user): implement get user by id

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,14 +1,36 @@
 const db = require("../querries");
 
-exports.getUsers = async (req, res, next) => {
+exports.getUsers = async (req, res) => {
   const response = await db.query("SELECT * FROM users ORDER BY id ASC");
-  const users = response.rows[0];
+  const users = response.rows;
 
   res.status(200).json({
     status: "success",
     rows: response.rowCount,
     data: {
       users,
+    },
+  });
+};
+
+exports.getUser = async (req, res) => {
+  const response = await db.query(
+    `SELECT * FROM users WHERE id =${req.params.id}`
+  );
+
+  const [user] = response.rows;
+  if (!user) {
+    return res.status(404).json({
+      status: "error",
+      message: "User not found.",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    rows: response.rowCount,
+    data: {
+      user,
     },
   });
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { getUsers } = require("../controller/user.controller");
+const { getUsers, getUser } = require("../controller/user.controller");
 
 router.get("/", getUsers);
+router.get("/:id", getUser);
 
 module.exports = router;
